5fd67dd014f52cafb5044501eb58d08db175d92a
Yi HAN <hanyi@8110@gmail.com>
2020-05-19 18:04:03 +0200

    HTTP server: dirty manage routes

--- appHTTP.js
+++ appHTTP.js
@@ -1,9 +1,19 @@
 const http = require("http");
 
-const server = http.createServer();
-server.on("connection", socket => {
-    console.log("new client connected");
-});
+const server = http.createServer(function (req, res) {
+    if(req.url === '/'){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write('Hello World ....!');
+        res.end();
+    }
+
+    if(req.url === '/api/courses'){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify([1,2,3]));
+        res.end();
+    }
+
+  });
 
 const port = 3000;
 server.listen(port);
