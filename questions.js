045c349dfecc7864236a67d5a80473d80e73e4c8
Yi HAN <hanyi@8110@gmail.com>
2020-05-22 10:07:56 +0200

    Separate to modules

--- questions.js
+++ questions.js
@@ -1,8 +1,8 @@
 // I have a list of questions
 // ask them one by one, record answers
 // display all answers when I replied all the questions
+const collectAnswer = require("./lib/collectAnswers");
 
-const readline = require("readline");
 
 const questions = [
     "How old are you?",
@@ -10,32 +10,8 @@ const questions = [
     "What are you going to do with Node.js?"
 ];
 
-
-answer(questions, (answers) => {
+collectAnswer(questions, (answers) => {
     console.log("Thank you for your answers. ");
     console.log(answers);
     process.exit();
 });
-
-function answer(questions, done ) {
-
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-
-    const answers = [];
-
-    const questionAnswered =  answer => {
-        answers.push(answer);
-        if(answers.length < questions.length){
-            rl.question(questions[answers.length], questionAnswered);
-        }
-        else{
-            done(answers);
-        }
-        
-    };
-
-    rl.question(questions[0], questionAnswered);
-}
