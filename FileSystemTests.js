cd885110df8af107c35a36a5a62d10b406189c06
Yi HAN <hanyi@8110@gmail.com>
2020-05-22 11:23:43 +0200

    If no explicit error handling, error will be swollen

--- FileSystemTests.js
+++ FileSystemTests.js
@@ -6,10 +6,10 @@ const fs = require("fs")
 // })
 
 
-fs.readdir('./', (err, files) => {
+fs.readdir('./assets2', (err, files) => {
     // Either err or files is null
-    if(err)
-        console.log(err);
-    else
+    // if(err)
+    //     console.log(err);
+    // else
         console.log(files);
-})
+}); //If no explicit error handling, error will be swollen
