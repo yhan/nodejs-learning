61bb5a2b091d602040892ceb1f3bdd7203abc3ec
Yi HAN <hanyi@8110@gmail.com>
2020-05-23 16:52:47 +0200

    Slow down streaming

--- duplexStream.js
+++ duplexStream.js
@@ -1,13 +1,11 @@
 const { createReadStream, createWriteStream } = require('fs');
-const {PassThrough} = require("stream");
+const {PassThrough, Duplex } = require("stream");
 const logUpdate = require('log-update');
 
 const report = new PassThrough();
 
 const readStream = createReadStream('./assets/powder-day.mp4');
 const writeStream = createWriteStream('./assets/copy.mp4');
-
-
 
 var size = 0;
 report.on("data", chunk => {
@@ -16,7 +14,29 @@ report.on("data", chunk => {
     logUpdate(size);
 });
 
+class Delay extends Duplex {
+    constructor(wait){
+        super();
+        this.wait = wait;
+    }
+
+    _write(chunk, encoding, callback){
+        setTimeout(callback, this.wait);
+        this.push(chunk);
+    }
+
+    _read(){
+    }
+
+   _final(){
+       this.push(null);
+   }
+}
+
+
+const delay = new Delay(20);
 
 readStream
+    .pipe(delay)
     .pipe(report)
     .pipe(writeStream);
